b07a7562d80beaa7e3aacb7a60775c46954dc9fb
Jelan Domingo <jrcd2006@gmail.com>
2023-12-06 11:26:38 -1000

    nuh uh

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,7 @@ import Link from "next/link";
 
 import { api } from "~/utils/api";
 
-export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
+export default function Home() {  const hello = api.post.hello.useQuery({ text: "from tRPC" });
 
   return (
     <>
@@ -19,17 +18,7 @@ export default function Home() {
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">One page →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
+          <Link href="/edits/landing">this page</Link>
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
               href="https://create.t3.gg/en/introduction"
@@ -49,4 +38,6 @@ export default function Home() {
       </main>
     </>
   );
-}
+  
+  }
+
