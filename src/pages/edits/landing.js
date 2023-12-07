9b782aeb414539dfd7af1ff7f89b38ef87b791d2
Jelan Domingo <jrcd2006@gmail.com>
2023-12-07 13:24:44 -1000

    Changing stuff real quick

--- src/pages/edits/landing.js
+++ src/pages/edits/landing.js
@@ -3,10 +3,16 @@ import Link from 'next/link';
 export default function FirstPost() {
   return (
     <>
-      <h1>First Post</h1>
+      <h1>Sighting Page</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
+      <h3>
+        Big Island
+      </h3>
+      <h3>
+        Oahu
+      </h3>
     </>
   );
 }
