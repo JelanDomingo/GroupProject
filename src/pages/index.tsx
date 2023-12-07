a98ef1b7d1ff4e4c1fdef0e0ae30b1ad868b2b75
Jelan Domingo <jrcd2006@gmail.com>
2023-12-07 13:15:19 -1000

    buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,16 +15,23 @@ export default function Home() {  const hello = api.post.hello.useQuery({ text: 
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            black <span className="text-[hsl(280,100%,70%)]">T3</span> App
+            I HATE SEA TURTLES
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link href="/edits/landing">this page</Link>
+          <Link
+              href="/edits/landing"
+            >
+              <h3 className="text-2xl font-bold">Sighting? →</h3>
+              <div className="text-lg">
+                le animal is not in any danger
+              </div>
+            </Link>
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
               href="https://create.t3.gg/en/introduction"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">Other page →</h3>
+              <h3 className="text-2xl font-bold">Emergency? →</h3>
               <div className="text-lg">
                 Learn more about Create T3 App, the libraries it uses, and how
                 to deploy it.
