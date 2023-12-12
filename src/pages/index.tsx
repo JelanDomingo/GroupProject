1784524166accfb961d0bb3c54c771d942028615
Jelan Domingo <jrcd2006@gmail.com>
2023-12-12 11:47:17 -1000

    test 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,13 +13,13 @@ export default function Home() {  const hello = api.post.hello.useQuery({ text: 
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-20 px-4 py-16 ">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             I HATE SEA TURTLES
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              className="flex max-w-xs flex-col gap-10 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
               href="/edits/landing"
               target="_blank"
             >
