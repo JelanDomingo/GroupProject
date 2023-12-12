524e241c7b49a4970a750376dca3e300747d6bfd
Jelan Domingo <jrcd2006@gmail.com>
2023-12-12 12:29:41 -1000

    i see your trueeeee colorssss shining throughhhhhh

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 
-import { api } from "~/utils/api";
-
-export default function Home() {  const hello = api.post.hello.useQuery({ text: "from tRPC" });
+export default function Home() {
 
   return (
     <>
