89c6a5bb0be5e7dd927a6d430dc61433c0435cf9
Jelan Domingo <jrcd2006@gmail.com>
2023-12-12 12:35:42 -1000

    merge conflict error?

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,8 @@ export default {
     extend: {},
   },
   plugins: [],
-<<<<<<< HEAD
-}
-=======
 }
 module.exports = {
   //...
   plugins: [require("daisyui")],
 }
->>>>>>> 13c3a8ff7ad9de51df65471fea1aac584933aa4e
