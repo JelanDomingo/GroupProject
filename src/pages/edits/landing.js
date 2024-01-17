9007a3780329bbe5507d1baf5e5f4cb7535dc9d5
[firstname <jrcd2006@gmail.com>
2024-01-17 11:53:59 -1000

    PLEASE

--- src/pages/edits/landing.js
+++ src/pages/edits/landing.js
@@ -13,6 +13,13 @@ export default function FirstPost() {
     </div>
   </div>
 </div>
+<details className="dropdown">
+  <summary className="m-1 btn">open or close</summary>
+  <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+    <li><a>Item 1</a></li>
+    <li><a>Item 2</a></li>
+  </ul>
+</details>
       <h1>Sighting Page</h1>
       <h2>
         <Link href="/">Back to home</Link>
