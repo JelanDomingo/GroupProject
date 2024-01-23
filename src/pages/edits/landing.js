861fc68580574437f04b67311ad79ee3e7c760e6
Jelan Domingo <jrcd2006@gmail.com>
2024-01-22 14:09:01 -1000

    Changed buttons and deleted stuff that is saved in my google doc

--- src/pages/edits/landing.js
+++ src/pages/edits/landing.js
@@ -21,26 +21,12 @@ export default function FirstPost() {
       <h3>
         Big Island
       </h3>
-      <Link
-              className="flex max-w-xs flex-col gap-10 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="/edits/logan"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">logan? →</h3>
-              <div className="text-lg">
-              </div>
-            </Link>
-      <h3>
-      <Link
-              className="flex max-w-xs flex-col gap-10 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="/edits/jelingling"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">jelan? →</h3>
-              <div className="text-lg">
-              </div>
-            </Link>
-      </h3>
+      <h2>
+        <Link href="/edits/jelingling">Jelan</Link>
+      </h2>
+      <h2>
+        <Link href="/edits/logan">Logan</Link>
+      </h2>
     </>
   );
 }
