d03948b14c9c2c2d0542eac73456068cd9dd9cc9
Jelan Domingo <jrcd2006@gmail.com>
2024-01-22 14:02:39 -1000

    Adding Display Pages

--- src/pages/edits/jelingling.js
+++ src/pages/edits/jelingling.js
@@ -1,1 +1,11 @@
 import Link from 'next/link';
+export default function Page() {
+    return(
+        <>
+        <h1>The thing</h1>
+        <h2>
+            <Link href="/">Back to home</Link>
+        </h2>
+        </>
+    );
+}
