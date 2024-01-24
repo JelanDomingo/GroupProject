1058ac202ea9eb9a813b9e1ba58a02be708bd0da
[firstname <jrcd2006@gmail.com>
2024-01-24 11:36:23 -1000

    Fixed

--- src/pages/edits/logan.js
+++ src/pages/edits/logan.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 export default function Page() {
     return(
         <>
-        <h1>The thing</h1>
+        <h1>Logan's Page</h1>
         <button className = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
             <Link href="/edits/landing">Back</Link>
         </button>
