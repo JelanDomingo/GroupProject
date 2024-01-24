68e3fad8cc81f5082cd191ca7583c5762ecaeb90
[firstname <jrcd2006@gmail.com>
2024-01-24 10:59:25 -1000

    COLORED BUTTON

--- src/pages/edits/logan.js
+++ src/pages/edits/logan.js
@@ -3,7 +3,7 @@ export default function Page() {
     return(
         <>
         <h1>The thing</h1>
-        <button className = "btn">
+        <button className = "btn btn-active btn-primary">
             <Link href="/edits/landing">Back</Link>
         </button>
         
