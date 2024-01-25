713ca02422bc541b448653241090bebd62ca6a2f
Jelan Domingo <jrcd2006@gmail.com>
2024-01-25 12:57:39 -1000

    link test

--- src/pages/edits/logan.js
+++ src/pages/edits/logan.js
@@ -8,6 +8,16 @@ export default function Page() {
             <Link href="/">Back to home</Link>
         </button>
         </h2>
+        <Link
+              className="flex max-w-xs flex-col gap-10 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              href="/edits/landing"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold">Sighting? →</h3>
+              <div className="text-lg">
+                le animal is not in any danger
+              </div>
+            </Link>
 
         
         </>
