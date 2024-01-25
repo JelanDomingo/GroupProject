5094b579d6967f79a10c06b881b9dd8c07270021
Jelan Domingo <jrcd2006@gmail.com>
2024-01-25 12:52:24 -1000

    button fixer?

--- src/pages/edits/logan.js
+++ src/pages/edits/logan.js
@@ -4,8 +4,9 @@ export default function Page() {
         <>
         <h1>Logan's Page</h1>
         <button className = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-            <Link href="/edits/landing">Back</Link>
+            <Link href="/">Back to home</Link>
         </button>
+
         
         </>
     );
