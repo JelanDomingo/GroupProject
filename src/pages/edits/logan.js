79bea9ffb0439f9e69bdcaa976870b7d966d0140
Jelan Domingo <jrcd2006@gmail.com>
2024-01-26 12:52:50 -1000

    chagne the cololors

--- src/pages/edits/logan.js
+++ src/pages/edits/logan.js
@@ -4,7 +4,7 @@ export default function Page() {
         <>
         <h1>Logan's Page</h1>
 
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#6fa8dc] to-[#0b5394]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             I HATE SEA TURTLES
