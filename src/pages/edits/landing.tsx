f3516e692d9d10ba9559c86c97a82b717bf60f1b
Jelan Domingo <jrcd2006@gmail.com>
2024-02-02 12:56:32 -1000

    pluh

--- src/pages/edits/landing.tsx
+++ src/pages/edits/landing.tsx
@@ -82,11 +82,6 @@ export default function FirstPost() {  const hello = api.post.hello.useQuery({ t
           </div>
           
         </div>
-
-      </main>
-
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#6fa8dc] to-[#0b5394]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
               className="flex max-w-xs flex-col gap-10 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
@@ -99,8 +94,6 @@ export default function FirstPost() {  const hello = api.post.hello.useQuery({ t
               </div>
             </Link>
           </div>
-          
-        </div>
 
       </main>
     </>
