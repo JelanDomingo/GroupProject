d78b23bb096e8dfb4b9b43ee3484f436aa55746d
Jelan Domingo <jrcd2006@gmail.com>
2024-02-02 13:23:28 -1000

    pluh

--- src/pages/edits/landing.tsx
+++ src/pages/edits/landing.tsx
@@ -92,18 +92,7 @@ export default function FirstPost() {  const hello = api.post.hello.useQuery({ t
           </div>
           
         </div>
-          <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:gap-16">
-          <Link
-              className="flex max-w-xs flex-col gap-10 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="/edits/logan"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Emergency? →</h3>
-              <div className="text-lg">
-                le animal is in mucho danger
-              </div>
-            </Link>
-          </div>
+          
 
       </main>
     </>
