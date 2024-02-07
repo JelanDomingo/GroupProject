95154fc8acc555b690d302d7828c9cd7e570eddd
[firstname <jrcd2006@gmail.com>
2024-02-07 11:09:11 -1000

    Tried making my page look somewhat nicer

--- src/pages/edits/sighting_landing.js
+++ src/pages/edits/sighting_landing.js
@@ -11,11 +11,16 @@ export default function Page() {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Sighting
           </h1>
+          <h3 className="text-5x1 font-extrabold tracking-tight text-white sm:text -[5rem]">
+            Welcome to the Sighting Page! Select the island from the following dropdown
+          </h3>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <details className="dropdown">
-            <summary className="m-1 btn">open or close</summary>
+            <summary className="m-1 btn text-5x1 tracking-tight text-white sm:text">Select your Island</summary>
             <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-              <li><a><Link href = "/edits/islands/big_island">Big Island</Link></a></li>
+              <li><a><Link 
+              className="flex max-w-xs flex-col gap-10 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              href = "/edits/islands/big_island">Big Island</Link></a></li>
               <li><a>Maui</a></li>
               <li><a>O'ahu</a></li>
               <li><a>Kauai</a></li>
